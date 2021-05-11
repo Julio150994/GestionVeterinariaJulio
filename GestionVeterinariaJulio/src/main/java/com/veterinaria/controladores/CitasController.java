1198cb04ad94c1988b9824f7adf27f0a50522cc0
Julio Muñoz <49689911+Julio1594@users.noreply.github.com>
2021-05-11 14:19:53 +0200

    Entrega3

--- GestionVeterinariaJulio/src/main/java/com/veterinaria/controladores/CitasController.java
+++ GestionVeterinariaJulio/src/main/java/com/veterinaria/controladores/CitasController.java
@@ -252,13 +252,14 @@ public class CitasController {
 			Usuarios usuario = usuariosRepository.findByUsername(auth.getName());
 			mavCitas.addObject("usuario",usuario.getId());
 		    
-			mavCitas.addObject("veterinarioActual",usuario.getUsername().toUpperCase());
-			
-			
 		    String fechaActual = anio+"-"+(mes+1)+"-"+dia;
-		    Date diaActual = Date.valueOf(fechaActual);// convertimos de cadena a fecha
+		    
+		    Date diaActual = Date.valueOf(fechaActual);// convertimos a fecha para la base de datos
 		    
 			mavCitas.addObject("citasTxt","No se han encontrado citas para el día actual");
+			
+			String fechaTxt = dia+"/"+(mes+1)+"/"+anio;
+			mavCitas.addObject("fechaActual",fechaTxt);
 			
 			mavCitas.addObject("citas",citasRepository.listarCitasDiaActual(diaActual,usuario.getId()));
 		}
