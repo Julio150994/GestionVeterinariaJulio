155d15a2fa375a35d165bf1c01c3d1a8f835712b
Julio Muñoz <49689911+Julio1594@users.noreply.github.com>
2021-05-19 02:18:28 +0200

    Entrega4

--- GestionVeterinariaJulio/src/main/java/com/veterinaria/configuraciones/ExportarDatosCliente.java
+++ GestionVeterinariaJulio/src/main/java/com/veterinaria/configuraciones/ExportarDatosCliente.java
@@ -221,7 +221,7 @@ public class ExportarDatosCliente {
 		//--------------Para las citas de la mascota---------------
 		PdfPTable tablaCitasMascota = new PdfPTable(12);
 		tablaCitasMascota.setWidthPercentage(100f);
-		tablaCitasMascota.setWidths(new float[] {1.3f,2f,1.33f,1.5f,1.15f,1.8f,1.8f,1.65f,1.5f,1.5f,1.13f,1.5f});// dimensiones para las 12 columnas con proporción 1/3 y 2/3
+		tablaCitasMascota.setWidths(new float[] {1f,2f,1f,2f,1f,2f,1f,2f,1f,2f,1f,2f});// dimensiones para las 12 columnas con proporción 1/3 y 2/3
 		tablaCitasMascota.setSpacingBefore(63.8f);
 		
 		this.headCitasMascota(tablaCitasMascota);
