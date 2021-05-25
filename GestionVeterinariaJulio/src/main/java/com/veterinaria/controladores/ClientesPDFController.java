ec20a7e9d83c032526085d474c9785d152f387a8
Julio Muñoz <49689911+Julio1594@users.noreply.github.com>
2021-05-25 13:27:37 +0200

    Entrega4

--- GestionVeterinariaJulio/src/main/java/com/veterinaria/controladores/ClientesPDFController.java
+++ GestionVeterinariaJulio/src/main/java/com/veterinaria/controladores/ClientesPDFController.java
@@ -63,7 +63,7 @@ public class ClientesPDFController {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if(auth.getPrincipal() != "anonymousUser") {
 			Usuarios cliente = usuariosRepository.findByUsername(auth.getName());
-			mavDatosCliente.addObject("citasTxt",cliente.getUsername()+" no tiene mascotas registradas en la base de datos");
+			mavDatosCliente.addObject("txtMascota",cliente.getUsername()+" no tiene mascotas registradas en la base de datos");
 			
 			mavDatosCliente.addObject("usuario",usuariosImpl.buscarId(cliente.getId()));
 			
