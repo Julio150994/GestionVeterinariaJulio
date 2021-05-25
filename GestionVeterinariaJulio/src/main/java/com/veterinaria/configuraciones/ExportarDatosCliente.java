cfeeb03cb34a5c4706588dee0e95c3b9959cb433
Julio Muñoz <49689911+Julio1594@users.noreply.github.com>
2021-05-25 11:13:19 +0200

    Entrega4

--- GestionVeterinariaJulio/src/main/java/com/veterinaria/configuraciones/ExportarDatosCliente.java
+++ GestionVeterinariaJulio/src/main/java/com/veterinaria/configuraciones/ExportarDatosCliente.java
@@ -190,7 +190,7 @@ public class ExportarDatosCliente {
 		
 		try {
 			/*---------------Para mostrar todas las citas de la mascota------------------*/
-			for(Citas cita: datosCitasCliente) {
+			for(Citas cita: datosCitasCliente) {				
 				PdfPTable tablaCitasMascota = new PdfPTable(12);
 				tablaCitasMascota.setWidthPercentage(90.1f);
 				tablaCitasMascota.setSpacingBefore(4.36f);
@@ -374,24 +374,6 @@ public class ExportarDatosCliente {
 				tablaCitasMascota.addCell(celdasCitasMascota);
 				
 				docCliente.add(tablaCitasMascota);
-				
-				//------------Ponemos la cabecera del pdf cada vez que pasamos de página-----------------
-				txtLogo = FontFactory.getFont("CALIBRI");
-				txtLogo.setSize(12);
-				txtLogo.setColor(Color.BLACK);
-				logo = new Paragraph("Informe de clínica DAM",txtLogo);
-				logo.setAlignment(Paragraph.ALIGN_LEFT);
-				docCliente.add(logo);
-				
-				logoClinica = Image.getInstance("src/main/resources/static/images/logo.png");
-				logoClinica.setAbsolutePosition(525,723);
-				logoClinica.setAlignment(Element.ALIGN_RIGHT);
-				
-				Paragraph linea = new Paragraph();// establecemos un párrafo como espacios en blanco
-				linea.setSpacingBefore(68.36f);
-				docCliente.add(linea);
-				
-				docCliente.add(logoClinica);
 				
 				docCliente.newPage();
 			}
