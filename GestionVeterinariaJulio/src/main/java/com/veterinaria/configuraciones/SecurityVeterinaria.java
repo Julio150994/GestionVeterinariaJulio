02c432b002628ec280ddef140f6b894162f13efa
Julio Muñoz <49689911+Julio1594@users.noreply.github.com>
2021-06-13 22:52:46 +0200

    Entrega5

--- GestionVeterinariaJulio/src/main/java/com/veterinaria/configuraciones/SecurityVeterinaria.java
+++ GestionVeterinariaJulio/src/main/java/com/veterinaria/configuraciones/SecurityVeterinaria.java
@@ -50,7 +50,7 @@ public class SecurityVeterinaria extends WebSecurityConfigurerAdapter {
 		.and()
 		.csrf().disable()
 		.authorizeRequests()
-			.antMatchers("/","/auth/**","/build/**","/css/**","/error/**","/clientes/**","/veterinarios/**","/frontend/**","/images/**","/mascotasImg/**","/js/**","/maps/**","/vendors/**","/webjars/**").permitAll()
+			.antMatchers("/","/auth/**","/build/**","/css/**","/error/**","clientes/**","/veterinarios/**","/frontend/**","/images/**","/mascotasImg/**","/js/**","/maps/**","/vendors/**","/webjars/**").permitAll()
 			.antMatchers("/menu","/registrarCliente","/auth/registrarCliente","/auth/login").permitAll()
 			.antMatchers("clientes/listadoClientes","clientes/formCliente","clientes/formCliente/{id}","clientes/saveCliente",
 					"clientes/eliminarCliente/{id}","clientes/mostrarCliente").access("hasRole('ROLE_ADMIN')")
